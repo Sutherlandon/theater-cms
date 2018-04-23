23e782ea2a566de06a8b1918aa5d540a2a35fe68
Sutherland, Landon Dexter <sutherlandon@gatech.edu>
2018-04-23 13:17:14 -0600

    Initial Commit, includes express api and the movie-grid component

---
+++ app.js
@@ -0,0 +1,39 @@
+/**
+ * Author: Landon Sutherland
+ * This is the server for the application.
+ */
+
+// import required packages
+var express = require('express'),
+    path = require('path'),
+    imdb = require('imdb-api');
+
+// build an express app
+var app = express();
+
+// serves public files like html, css, js, and img
+app.use(express.static(path.join(__dirname, '/public')));
+
+// gets movie meta-data based on title from the imbd-api
+app.get('/movie/:title', function (req, res) {
+  imdb.getReq({
+    name: req.params.title
+  }, (err, data) => {
+    res.send(data);
+  });
+});
+
+// gets movie meta-data based on title from the imbd-api
+app.get('/movie/:title/:year', function (req, res) {
+  imdb.getReq({
+    name: req.params.title,
+    year: req.params.year
+  }, (err, data) => {
+    res.send(data);
+  });
+});
+
+// fires up the server to listen on port 3000
+app.listen(3000, function () {
+  console.log('ReelDeal running (port: 3000)');
+});
---
+++ src/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Movie(props) {
+  return (
+    <div className="movie-contianer" key={props.key}>
+      <table className="pure-table pure-table-borderd pure-table-striped movie-table">
+        <tbody>
+          <tr>
+            <td colSpan="2" className="movie-poster">
+              <img src={props.poster} alt={props.name}/>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="2">{props.name}</td>
+          </tr>
+          <tr>
+            <td className="rating">{props.rating}</td>
+            <td>{props.runtime}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+class MovieGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.movies = [{
+      "name" : "Star Wars: Rogue One",
+      "poster": "img/star_wars.jpg",
+      "rating" : "PG-13",
+      "runtime" : "2h 13m",
+      "showtimes" : {
+        "Friday 1/06" : [
+          "12:45 2D",
+          "3:45 2D",
+          "6:45 3D",
+          "9:45 3D"
+        ],
+        "Saturday 1/07" : [
+          "12:45 2D",
+          "3:45 2D",
+          "6:45 3D",
+          "9:45 3D"
+        ]
+      }
+    }, {
+      "name" : "Assasins Creed",
+      "poster": "img/assassins_creed.jpg",
+      "rating" : "PG-13",
+      "runtime" : "2h 20m",
+      "showtimes" : {
+        "Friday 1/06" : [
+          "1:00 2D",
+          "4:00 2D",
+          "7:00 3D",
+          "9:40 3D"
+        ],
+        "Saturday 1/07" : [
+          "1:00 2D",
+          "4:00 2D",
+          "7:00 3D",
+          "9:40 3D"
+        ]
+      }
+    }];
+  }
+
+  renderMovie(movie) {
+    return (
+      <Movie
+        name={movie.name}
+        poster={movie.poster}
+        rating={movie.rating}
+        runtime={movie.runtime}
+      />
+    );
+  }
+
+  render(props) {
+    return (
+      this.movies.map((movie) => this.renderMovie(movie))
+    );
+  }
+}
+
+ReactDOM.render(
+  <MovieGrid />,
+  document.getElementById("root")
+);
