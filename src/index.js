775e1a6e40326e2515283741b3d4cc15a8efb1fa
Sutherland, Landon Dexter <sutherlandon@gatech.edu>
2018-04-25 00:58:13 -0600

    realdeal css now less, will be moved to grid, and probably renamed.  Movie cards completed.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Movie(props) {
+function MovieCard(props) {
+  const showtimes = props.showtimes.map((time, i) => {
+    return (
+      <span key={i}>{time}</span>
+    )
+  });
+  const rating_class = "rating " + props.rating;
+
+  return (
+    <div class="movie-card">
+      <div class="poster">
+        <img src={props.poster} alt={props.name}/>
+      </div>
+      <div class="info">
+        <span class={rating_class}>{props.rating}</span>
+        <span class="runtime">{props.runtime}</span>
+      </div>
+      <div class="showtimes">{showtimes}</div>
+    </div>
+  )
+  /*
   return (
     <table className="pure-table pure-table-borderd pure-table-striped movie-table">
       <tbody>
@@ -20,6 +40,7 @@ function Movie(props) {
       </tbody>
     </table>
   );
+  */
 }
 
 class MovieGrid extends React.Component {
@@ -47,7 +68,45 @@ class MovieGrid extends React.Component {
     }, {
       "name" : "Assasins Creed",
       "poster": "img/assassins_creed.jpg",
+      "rating" : "R",
+      "runtime" : "2h 20m",
+      "showtimes" : {
+        "Friday 1/06" : [
+          "1:00 2D",
+          "4:00 2D",
+          "7:00 3D",
+          "9:40 3D"
+        ],
+        "Saturday 1/07" : [
+          "1:00 2D",
+          "4:00 2D",
+          "7:00 3D",
+          "9:40 3D"
+        ]
+      }
+    }, {
+      "name" : "Star Wars: Rogue One",
+      "poster": "img/star_wars.jpg",
       "rating" : "PG-13",
+      "runtime" : "2h 13m",
+      "showtimes" : {
+        "Friday 1/06" : [
+          "12:45 2D",
+          "3:45 2D",
+          "6:45 3D",
+          "9:45 3D"
+        ],
+        "Saturday 1/07" : [
+          "12:45 2D",
+          "3:45 2D",
+          "6:45 3D",
+          "9:45 3D"
+        ]
+      }
+    }, {
+      "name" : "Assasins Creed",
+      "poster": "img/assassins_creed.jpg",
+      "rating" : "R",
       "runtime" : "2h 20m",
       "showtimes" : {
         "Friday 1/06" : [
@@ -73,11 +132,12 @@ class MovieGrid extends React.Component {
         {this.movies.map((movie, i) => {
           return (
             <div className="movie-contianer" key={i}>
-              <Movie
+              <MovieCard
                 name={movie.name}
                 poster={movie.poster}
                 rating={movie.rating}
                 runtime={movie.runtime}
+                showtimes={movie.showtimes["Friday 1/06"]}
               />
             </div>
           );
