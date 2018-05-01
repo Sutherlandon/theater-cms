60825dc63585d50e4c13b902e09f6b5693ea6d98
Landon Sutherland <Sutherlandon@gmail.com>
2018-05-01 12:33:41 -0600

    Fixed some formatting stuff with Theater Info title and went back downt to 4 movies.

--- src/index.js
+++ src/index.js
@@ -132,24 +132,6 @@ class MovieGrid extends React.Component {
         ]
       }
     }, {
-      "name" : "Star Wars: Rogue One",
-      "poster": "img/star_wars.jpg",
-      "rating" : "PG-13",
-      "runtime" : "2h 13m",
-      "showtimes" : {
-        "Friday 1/06" : [
-          "3:45 2D",
-          "6:45 3D",
-          "9:45 3D"
-        ],
-        "Saturday 1/07" : [
-          "1:00 2D",
-          "4:00 2D",
-          "7:00 3D",
-          "9:40 3D"
-        ]
-      }
-    }, {
       "name" : "Assasins Creed",
       "poster": "img/assassins_creed.jpg",
       "rating" : "R",
